f443832b73a1a280d97dcf907fb1157ef2d3320e
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2026-05-14 14:10:36 -0500

    Add additional copy buttons

--- src/pages/UserGames.js
+++ src/pages/UserGames.js
@@ -152,13 +152,14 @@ class UserGames extends Component {
                                 const createdAt = this.getGameValue(game, 'createdAt');
                                 const boardState = this.getGameValue(game, 'boardState');
                                 const boardString = this.getGameValue(game, 'boardString');
+                                const matchId = this.getGameValue(game, 'matchId');
 
                                 return (
                                     <div
                                         key={index}
                                         className="bg-gray-800 border border-gray-700 rounded-lg flex flex-row"
                                     >
-                                        <div className="flex flex-col lg:w-1/3 p-4 border-r border-gray-700 space-y-2">
+                                        <div className="flex flex-col lg:w-1/3 p-4 border-r border-gray-700 gap-2">
                                             <div className="flex items-center justify-between gap-2">
                                                 <span className="text-white font-semibold">{name}</span>
                                                 <span className="text-gray-400">{getTeamName(team)}</span>
@@ -178,6 +179,15 @@ class UserGames extends Component {
                                             <p className="text-gray-500 text-xs">
                                                 {this.formatDate(createdAt)}
                                             </p>
+                                            <button
+                                                onClick={() => navigator.clipboard.writeText(matchId)}
+                                                className="p-1 w-fit mt-auto text-gray-400 rounded hover:bg-gray-700 transition-colors"
+                                                title="Copy Match ID"
+                                            >
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
+                                                </svg>
+                                            </button>
                                         </div>
                                         <div className="p-4 flex justify-center bg-gray-900/50 flex-1">
                                             <BingoCanvas
