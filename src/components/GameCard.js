4e33b899e656a069c5189a2912da6e94be572ede
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2026-06-20 22:45:48 -0500

    Add board state button

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -394,16 +394,28 @@ const GameCard = ({ game, idx, type }) => {
                 <span className="text-gray-400">Starting shelter: {startingShelter}<span className="text-gray-500 text-sm">{isRandomShelter === "na" ? "" : (isRandomShelter ? " (random)" : " (set)")}</span></span>
                 <span className="text-gray-400">Passage used: {passageUsed === "na" ? "" : (passageUsed ? "Yes" : "No")}</span>
                 <div className={`flex ${type === "ranked" ? "flex-row" : "flex-row h-full"}`}>
-                    <button
-                        onClick={() => navigator.clipboard.writeText(matchId)}
-                        className={`flex flex-row text-xs p-1 w-fit ${type === "ranked" ? "" : "mt-auto"} text-gray-400 rounded hover:bg-gray-700 transition-colors`}
-                        title="Copy Match ID"
-                    >
-                        <span className="mr-1 mt-1">Match ID</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
-                        </svg>
-                    </button>
+                    <div className="flex flex-row">
+                        <button
+                            onClick={() => navigator.clipboard.writeText(matchId)}
+                            className={`flex flex-row text-xs p-1 w-fit ${type === "ranked" ? "" : "mt-auto"} text-gray-400 rounded hover:bg-gray-700 transition-colors`}
+                            title="Copy Match ID"
+                        >
+                            <span className="mr-1 mt-1">Match ID</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
+                            </svg>
+                        </button>
+                        <button
+                            onClick={() => navigator.clipboard.writeText(boardState)}
+                            className={`flex flex-row text-xs p-1 w-fit ${type === "ranked" ? "" : "mt-auto"} text-gray-400 rounded hover:bg-gray-700 transition-colors`}
+                            title="Copy Board State"
+                        >
+                            <span className="mr-1 mt-1">Board State</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z" />
+                            </svg>
+                        </button>
+                    </div>
                     <p className="mt-auto ml-auto w-fit text-gray-500 text-xs">
                         {updatedAt ? formatDate(updatedAt) : formatDate(createdAt)}
                     </p>
