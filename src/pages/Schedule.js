21d3e4f11fcb992eab25bb62813f08229491cb45
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2026-07-18 02:31:43 -0700

    Fix schedule search

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -21,7 +21,7 @@ class Schedule extends Component {
 
     fetchTeams = async () => {
         try {
-            const response = await fetch('https://us-central1-bingo-db-57e75.cloudfunctions.net/api/teams');
+            const response = await fetch('https://us-central1-bingo-db-57e75.cloudfunctions.net/api/teams?min=0&max=1000');
             if (!response.ok) {
                 throw new Error(`API error: ${response.status}`);
             }
